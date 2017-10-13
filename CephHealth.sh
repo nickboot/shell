ac8d2fbd3dd822b3e3053a283e29a0142b713721
nickboot <nickbooty50@gmail.com>
2017-10-13 18:43:41 +0800

    ceph集群状态每秒记录到文件
    每行记录时间和集群状态

--- CephHealth.sh
+++ CephHealth.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
-while true;
+#!/usr/bin/bash
+
+# ceph集群状态每秒记录到文件
+# 时间 2017/10/13
+
+while true
 do
-/bin/sleep 1 
-ceph health >> /root/status.txt
-date >> /root/status.txt
+
+        t=`date`
+        s=`ceph health`
+        echo "$t -- $s" >> /ceph_status/ceph_status.txt
+        sleep 1
 done
+
+# nohup 忽略hangup信号，用户注销和断网，命令还继续执行；"&"命令放在后台执行
+# nohup ./ceph_status.sh & 
+# echo "$t -- $s" >> /ceph_status/ceph_status.txt
+# echo $t
+# s=`ceph health`
+# echo $s
